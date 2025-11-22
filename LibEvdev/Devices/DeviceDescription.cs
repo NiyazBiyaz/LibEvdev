a1a85af5e4bd95b02f1648440b1da619d09974f4
NiyaBiyaz <niyazik114422@gmail.com>
2025-11-22 22:58:56 +0500

    Add device description type

---
+++ LibEvdev/Devices/DeviceDescription.cs
@@ -0,0 +1,86 @@
+// Copyright (c) NiyazBiyaz <niyazik114422@gmail.com>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Diagnostics.CodeAnalysis;
+using LibEvdev.Native;
+
+namespace LibEvdev.Devices
+{
+    public record DeviceDescription
+    {
+        public string Name { get; init; }
+        public required IDictionary<IdProperty, int> Id { get; init; }
+        public int? DriverVersion { get; init; }
+        public string? Phys { get; init; }
+        public string? Uniq { get; init; }
+        public string? Path { get; init; }
+
+        public IDictionary<EventType, List<ushort>>? EventCapabilities { get; init; }
+
+        public DeviceDescription(IDevice device)
+        {
+            Name = device.Name;
+            Phys = device.Phys;
+            Uniq = device.Uniq;
+            Path = device.Path;
+            DriverVersion = device.DriverVersion;
+            Id = device.Id;
+
+            var eventTypes = device.GetSupportedEventTypes();
+            var capabilities = new Dictionary<EventType, List<ushort>>();
+            foreach (var type in eventTypes)
+            {
+                capabilities[type] = new();
+                // 0x2ff = KEY_MAX
+                for (ushort code = 1; code < 0x2ff; code++)
+                {
+                    if (device.HasEvent((uint)type, code))
+                        capabilities[type].Add(code);
+                }
+            }
+            EventCapabilities = capabilities;
+        }
+
+        public DeviceDescription(string name)
+        {
+            Name = name;
+        }
+
+        [SetsRequiredMembers]
+        public DeviceDescription(string name, IDictionary<IdProperty, int> id, IDictionary<EventType, List<ushort>> eventCapabilities)
+        {
+            Name = name;
+            Id = id;
+            EventCapabilities = eventCapabilities;
+        }
+
+        public readonly static DeviceDescription DEFAULT_MOUSE_CONFIG = new("LibEvdev mouse")
+        {
+            Id = new Dictionary<IdProperty, int>
+            {
+                {IdProperty.BusType, 0x03}, // BusType USB
+                {IdProperty.Product, 0x1440},
+                {IdProperty.Vendor, 0x30fa},
+                {IdProperty.Version, 1},
+            },
+            EventCapabilities = new Dictionary<EventType, List<ushort>>
+            {
+                {EventType.Synchronization, []},
+                {EventType.Key, [
+                    (ushort)Key.ButtonLeft,
+                    (ushort)Key.ButtonRight,
+                    (ushort)Key.ButtonMiddle,
+                    (ushort)Key.ButtonSide,
+                    (ushort)Key.ButtonExtra
+                ]},
+                {EventType.Relative, [
+                    (ushort)RelativeAxis.X,
+                    (ushort)RelativeAxis.Y,
+                    (ushort)RelativeAxis.Wheel,
+                    (ushort)RelativeAxis.WheelHighResolution,
+                ]},
+                {EventType.Miscellaneous, [ (ushort)Miscellaneous.ScanCode ]}
+            }
+        };
+    }
+}
