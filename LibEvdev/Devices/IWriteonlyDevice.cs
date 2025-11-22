7614fc57da3677eb83fb93bc314d949f3a1cbf59
NiyaBiyaz <niyazik114422@gmail.com>
2025-11-22 22:59:27 +0500

    Add WriteonlyDevice class and his interface

---
+++ LibEvdev/Devices/IWriteonlyDevice.cs
@@ -0,0 +1,77 @@
+// Copyright (c) NiyazBiyaz <niyazik114422@gmail.com>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using LibEvdev.Native;
+
+namespace LibEvdev.Devices
+{
+    public interface IWriteonlyDevice : IDevice
+    {
+        public void SetName(string name);
+
+        public void SetId(IDictionary<IdProperty, int> idDict);
+
+        /// <summary>
+        /// Enable specified <paramref name="type"/> for this device for allow using by it.
+        /// </summary>
+        /// <param name="type">Type for enabling.</param>
+        public void Enable(EventType type);
+
+        /// <summary>
+        /// Enable specified <paramref name="code"/> of the <paramref name="type"/>
+        /// for this device for allow using by it.
+        /// </summary>
+        /// <param name="type">Type that for the <paramref name="code"/>.</param>
+        /// <param name="code">Code of the <paramref name="type"/> for enabling.</param>
+        public void Enable(EventType type, uint code);
+
+        /// <summary>
+        /// Disable specified <paramref name="type"/> for this device, so it's will can't
+        /// be used after it without <see cref="Enable"/>
+        /// </summary>
+        /// <param name="type">Type for disabling.</param>
+        public void Disable(EventType type);
+
+        /// <summary>
+        /// Disable specified <paramref name="code"/> of the <paramref name="type"/> for this device.
+        /// </summary>
+        /// <param name="type">Type of the <paramref name="code"/>.</param>
+        /// <param name="code">Code for disabling.</param>
+        public void Disable(EventType type, uint code);
+
+        /// <summary>
+        /// Write only <paramref name="inputEvent"/> to the device descriptor. Needs <see cref="Flush"/>
+        /// for not miss the synchronization with kernel.
+        /// </summary>
+        /// <param name="inputEvent"></param>
+        public void Write(InputEvent inputEvent);
+
+        /// <summary>
+        /// <see cref="Write"/> synchronization event to the device.
+        /// </summary>
+        public void Flush()
+        {
+            Write(new InputEvent(EventType.Synchronization, 0, 0));
+        }
+
+        /// <summary>
+        /// <see cref="Write"/> one event and flush it.
+        /// </summary>
+        public void WriteFrame(InputEvent inputEvent)
+        {
+            Write(inputEvent);
+            Flush();
+        }
+
+        /// <summary>
+        /// <see cref="Write"/> some events and flush it.
+        /// </summary>
+        /// <param name="inputEvents"></param>
+        public void WriteFrame(IEnumerable<InputEvent> inputEvents)
+        {
+            foreach (var evt in inputEvents)
+                Write(evt);
+            Flush();
+        }
+    }
+}
---
+++ LibEvdev/Devices/WriteonlyDevice.cs
@@ -0,0 +1,74 @@
+// Copyright (c) NiyazBiyaz <niyazik114422@gmail.com>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using LibEvdev.Native;
+
+namespace LibEvdev.Devices
+{
+    public class WriteonlyDevice : Device, IWriteonlyDevice, IDisposable
+    {
+        private nint uiDev;
+
+        public WriteonlyDevice(DeviceDescription configuration)
+            : base()
+        {
+            ArgumentNullException.ThrowIfNull(configuration.EventCapabilities);
+
+            SetId(configuration.Id);
+            SetName(configuration.Name);
+
+            foreach (var type in configuration.EventCapabilities.Keys)
+            {
+                Enable(type);
+                foreach (uint code in configuration.EventCapabilities[type])
+                    Enable(type, code);
+            }
+
+            int err = Evdev.UinputCreateFromDevice(Dev, (int)UinputOpenMode.Managed, ref uiDev);
+            if (err != 0)
+                throw AutoExternalException.New(err);
+
+            Logger.Information("Created new device from scratch using configuration: {Config}", configuration);
+        }
+
+        public void SetName(string name)
+        {
+            ArgumentNullException.ThrowIfNull(name);
+            Evdev.SetName(Dev, name);
+        }
+
+        public void SetId(IDictionary<IdProperty, int> idDict)
+        {
+            if (idDict.TryGetValue(IdProperty.BusType, out int bus))
+                Evdev.SetIdBustype(Dev, bus);
+            if (idDict.TryGetValue(IdProperty.Vendor, out int vdr))
+                Evdev.SetIdVendor(Dev, vdr);
+            if (idDict.TryGetValue(IdProperty.Product, out int pro))
+                Evdev.SetIdProduct(Dev, pro);
+            if (idDict.TryGetValue(IdProperty.Version, out int ver))
+                Evdev.SetIdVersion(Dev, ver);
+        }
+
+        public void Enable(EventType type) => Evdev.EnableEventType(Dev, (uint)type);
+
+        // original function signature have an argument that not used in this method
+        // so we just place there a null
+        public unsafe void Enable(EventType type, uint code) => Evdev.EnableEventCode(Dev, (uint)type, code, null);
+
+        public void Disable(EventType type) => Evdev.DisableEventType(Dev, (uint)type);
+
+        public void Disable(EventType type, uint code) => Evdev.DisableEventCode(Dev, (uint)type, code);
+
+        public void Write(InputEvent inputEvent)
+        {
+            Evdev.UinputWriteEvent(uiDev, (uint)inputEvent.Type, inputEvent.Code, inputEvent.Value);
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+            Evdev.UinputDestroy(uiDev);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
