d24cba8c5bbff7eda75311c59453f7ab5c94667c
NiyazBiyaz <niyazik114422@gmail.com>
2025-11-27 18:14:02 +0500

    Implement interactive mode and activate it if 1 argument assigned

--- Evtest/Program.cs
+++ Evtest/Program.cs
@@ -2,6 +2,7 @@
 // See the LICENSE file in the repository root for full license text.
 
 using LibEvdev.Devices;
+using LibEvdev.Native;
 using Serilog;
 using Spectre.Console;
 
@@ -9,7 +10,7 @@ namespace Evtest
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                             .WriteTo.Console(outputTemplate:
@@ -17,18 +18,11 @@ namespace Evtest
                             .MinimumLevel.Error()
                             .CreateLogger();
 
-            if (args.Length > 0)
+            if (args.Length > 1)
             {
                 foreach (string pathName in args)
                 {
-                    if (!Device.IsValidDevicePath(pathName))
-                    {
-                        AnsiConsole.MarkupLine($"""
-                        [red]Received path [cyan]{pathName}[/] is not valid.[/]
-                        Try another like [bold]/dev/input/eventX[/].
-                        """);
-                        return;
-                    }
+                    validatePath(pathName);
 
                     using (var device = DeviceHelper.OpenReadOnly(pathName))
                     {
@@ -39,6 +33,11 @@ namespace Evtest
                         AnsiConsole.Write("\n");
                     }
                 }
+            }
+            else if (args.Length == 1)
+            {
+                string path = args.First();
+                await StartInteractiveMode(path);
             }
             else
             {
@@ -60,6 +59,60 @@ namespace Evtest
 
                 using (var device = DeviceHelper.OpenReadOnly(deviceToOpen))
                     DeviceInfoPrinter.WriteDeviceInfo(new DeviceDescription(device));
+            }
+        }
+
+        public async static Task StartInteractiveMode(string path)
+        {
+            validatePath(path);
+
+            CancellationTokenSource cts = new();
+            long eventsCount = 0;
+
+            Console.CancelKeyPress += (_, _) =>
+            {
+                cts.Cancel();
+                AnsiConsole.MarkupLine($"Events was read: [bold green]{eventsCount}[/]");
+            };
+
+            using (var device = DeviceHelper.OpenReadOnly(path))
+            {
+                var description = new DeviceDescription(device);
+                DeviceInfoPrinter.WriteDeviceInfo(description);
+
+                AnsiConsole.MarkupLine($"[bold]Start receiving events from device [green]{description.Name}[/][/]");
+
+                await foreach (var evt in device.ReadInputEventsAsync(100, cts.Token))
+                {
+                    eventsCount++;
+
+                    if (evt.Type == EventType.Synchronization)
+                    {
+                        AnsiConsole.MarkupLine("[bold]------------------Sync reported------------------[/]");
+                        continue;
+                    }
+
+                    DateTime timeStamp = evt.TimeValue.AsDateTime();
+                    string typeName = Evdev.GetEventTypeName(evt.Type);
+                    string codeName = Evdev.GetEventCodeName(evt.Type, evt.Code);
+
+                    AnsiConsole.MarkupLine($"[gray54]{timeStamp:t}[/] Received event: " +
+                                           $"type = [bold green]{evt.Type}[/] [purple]{typeName}[/] " +
+                                           $"code = [bold green]{evt.Code}[/] [purple]{codeName}[/] " +
+                                           $"value = [bold green]{evt.Value}[/]");
+                }
+            }
+        }
+
+        private static void validatePath(string path)
+        {
+            if (!Device.IsValidDevicePath(path))
+            {
+                AnsiConsole.MarkupLine($"""
+                [red]Received path [cyan]{path}[/] is not valid.[/]
+                Try another like [bold]/dev/input/eventX[/].
+                """);
+                return;
             }
         }
     }
